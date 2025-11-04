d548b75ed06aafb922e80e8e48d729f9186f813f
SuniL <sunilgumatimath.vs@gmail.com>
2025-11-04 12:19:43 +0530

    day 1: basic variables in py

---
+++ variables.py
@@ -0,0 +1,28 @@
+# Variables in Python
+first_name = "sunil"
+last_name = "ted"
+
+country = "India"
+city = "Bangalore"
+
+age = 25
+is_married = False
+
+
+skills = ["java", "python", "react", "JS"]
+person_info = {"firstname": "Sunil", "country": "Bharat", "city": "Alamatti"}
+
+print("First Name :", first_name)
+print("First Name length : ", len(first_name))
+
+print("Last Name :", last_name)
+print("Last Name length : ", len(last_name))
+
+print("Full Name : ", first_name + " " + last_name)
+print("Full name length :", len(first_name + last_name))
+
+print("Country :", country)
+print("Age : ", age)
+print("Married ? : ", is_married)
+print("Skills : ", skills[::])
+print("Person Info :", person_info)
