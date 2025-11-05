cab8888d84094d5261ccd05b8956a1b02f6e0543
SuniL <sunilgumatimath.vs@gmail.com>
2025-11-05 22:34:15 +0530

    feat: add string indexing and negative indexing examples
    - Add code examples for accessing characters in strings by positive and negative index
    - Improve comments and formatting in strings.py for better clarity
    - Expand the tutorial with practical string manipulation demonstrations

--- 03-strings/strings.py
+++ 03-strings/strings.py
@@ -1,13 +1,10 @@
-# Single line commet
-letter = 'p'
-
-print(letter)
-print(len(letter))
-
-greeting = 'Hello World'
-print(greeting)
-print(len(greeting))
-
+# Single line comment
+letter = 'P'                # A string could be a single character or a bunch of texts
+print(letter)               # P
+print(len(letter))          # 1
+greeting = 'Hello, World!'  # String could be  a single or double quote,"Hello, World!"
+print(greeting)             # Hello, World!
+print(len(greeting))        # 13
 sentence = "I hope you are enjoying 30 days of python challenge"
 print(sentence)
 
@@ -16,7 +13,6 @@ multiline_string = '''I am a teacher and enjoy teaching.
 I didn't find anything as rewarding as empowering people.
 That is why I created 30 days of python.'''
 print(multiline_string)
-
 # Another way of doing the same thing
 multiline_string = """I am a teacher and enjoy teaching.
 I didn't find anything as rewarding as empowering people.
@@ -24,21 +20,16 @@ That is why I created 30 days of python."""
 print(multiline_string)
 
 # String Concatenation
-
 first_name = 'sunil'
 last_name = 'ted'
 space = ' '
-
-full_name = first_name + space + last_name
-
-print(full_name)
-
+full_name = first_name  +  space + last_name
+print(full_name) # Asabeneh Yetayeh
+# Checking length of a string using len() builtin function
 print(len(first_name))  # 8
 print(len(last_name))   # 7
 print(len(first_name) > len(last_name)) # True
 print(len(full_name)) # 15
-
-
 
 #### Unpacking characters 
 language = 'Python'
@@ -49,3 +40,20 @@ print(c) # t
 print(d) # h
 print(e) # o
 print(f) # n
+
+# Accessing characters in strings by index
+language = 'Python'
+first_letter = language[0]
+print(first_letter) # P
+second_letter = language[1]
+print(second_letter) # y
+last_index = len(language) - 1
+last_letter = language[last_index]
+print(last_letter) # n
+
+# If we want to start from right end we can use negative indexing. -1 is the last index
+language = 'Python'
+last_letter = language[-1]
+print(last_letter) # n
+second_last = language[-2]
+print(second_last) # o
