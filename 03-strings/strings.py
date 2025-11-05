c571fecf8c4094b3de8d9aaefd02e0a4c527b0fa
SuniL <sunilgumatimath.vs@gmail.com>
2025-11-05 22:26:45 +0530

    feat(strings): add examples for string length and multiline strings
    Add code snippets demonstrating string operations including printing single-line and multiline strings, and calculating their lengths, to illustrate basic Python string handling in the 30 days of Python challenge.

--- 03-strings/strings.py
+++ 03-strings/strings.py
@@ -2,3 +2,23 @@
 letter = 'p'
 
 print(letter)
+print(len(letter))
+
+greeting = 'Hello World'
+print(greeting)
+print(len(greeting))
+
+sentence = "I hope you are enjoying 30 days of python challenge"
+print(sentence)
+
+# Multiline String
+multiline_string = '''I am a teacher and enjoy teaching.
+I didn't find anything as rewarding as empowering people.
+That is why I created 30 days of python.'''
+print(multiline_string)
+
+# Another way of doing the same thing
+multiline_string = """I am a teacher and enjoy teaching.
+I didn't find anything as rewarding as empowering people.
+That is why I created 30 days of python."""
+print(multiline_string)
