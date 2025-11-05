e252a80105e363f17326a715c369954e32048689
SuniL <sunilgumatimath.vs@gmail.com>
2025-11-05 22:29:31 +0530

    feat(strings): add examples for string concatenation and character unpacking
    - Added string concatenation example using first_name, last_name, and space variables
    - Included character unpacking demonstration with the 'Python' string
    - Enhanced the strings.py file with practical code snippets for learning purposes

--- 03-strings/strings.py
+++ 03-strings/strings.py
@@ -22,3 +22,30 @@ multiline_string = """I am a teacher and enjoy teaching.
 I didn't find anything as rewarding as empowering people.
 That is why I created 30 days of python."""
 print(multiline_string)
+
+# String Concatenation
+
+first_name = 'sunil'
+last_name = 'ted'
+space = ' '
+
+full_name = first_name + space + last_name
+
+print(full_name)
+
+print(len(first_name))  # 8
+print(len(last_name))   # 7
+print(len(first_name) > len(last_name)) # True
+print(len(full_name)) # 15
+
+
+
+#### Unpacking characters 
+language = 'Python'
+a,b,c,d,e,f = language # unpacking sequence characters into variables
+print(a) # P
+print(b) # y
+print(c) # t 
+print(d) # h
+print(e) # o
+print(f) # n
