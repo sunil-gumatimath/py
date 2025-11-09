b917e9d63699ff50d2f6254c673de4da1be7f6cd
SuniL <sunilgumatimath.vs@gmail.com>
2025-11-09 12:38:03 +0530

    chore: comment out list examples to reduce runtime noise
    - Comment out list initialization, printing, and slicing demo code
    - Keep examples available for reference without executing on import or run

--- 04-lists/lists.py
+++ 04-lists/lists.py
@@ -1,102 +1,115 @@
-empty_list: list[int] = []
-print(len(empty_list))
+# empty_list: list[int] = []
+# print(len(empty_list))
 
-fruits = ['banana', 'orange', 'mango', 'lemon']                         # list of fruits
-vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']          # list of vegetables
-animal_products = ['milk', 'meat', 'butter', 'yoghurt']                 # list of animal products
-web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongoDB']    # list of web technologies
-countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+# fruits = ['banana', 'orange', 'mango', 'lemon']                         # list of fruits
+# vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']          # list of vegetables
+# animal_products = ['milk', 'meat', 'butter', 'yoghurt']                 # list of animal products
+# web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongoDB']    # list of web technologies
+# countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
 
-# Print the lists and it length
-print('Fruits:', fruits)
-print('Number of fruits:', len(fruits))
-print('Vegetables:', vegetables)
-print('Number of vegetables:', len(vegetables))
-print('Animal products:',animal_products)
-print('Number of animal products:', len(animal_products))
-print('Web technologies:', web_techs)
-print('Number of web technologies:', len(web_techs))
-print('Number of countries:', len(countries))
-print('')
+# # Print the lists and it length
+# print('Fruits:', fruits)
+# print('Number of fruits:', len(fruits))
+# print('Vegetables:', vegetables)
+# print('Number of vegetables:', len(vegetables))
+# print('Animal products:',animal_products)
+# print('Number of animal products:', len(animal_products))
+# print('Web technologies:', web_techs)
+# print('Number of web technologies:', len(web_techs))
+# print('Number of countries:', len(countries))
+# print('')
 
 
-# Modifying list
-fruits = ['banana','orange','mango','lemon']
-first_fruit = fruits[0]
-print(first_fruit)
-second_fruit = fruits[1]
-print(second_fruit)
-last_fruit = fruits[-1]
-print(last_fruit)
+# # Modifying list
+# fruits = ['banana','orange','mango','lemon']
+# first_fruit = fruits[0]
+# print(first_fruit)
+# second_fruit = fruits[1]
+# print(second_fruit)
+# last_fruit = fruits[-1]
+# print(last_fruit)
 
-# Last index
-last_index = len(fruits) - 1
-print(last_index)
+# # Last index
+# last_index = len(fruits) - 1
+# print(last_index)
 
-print(' ')
+# print(' ')
 
-# Accessing items
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-last_fruit = fruits[-1]
-second_last = fruits[-2]
-print(last_fruit)                                                          # lemon
-print(second_last)                                                         # mango
+# # Accessing items
+# fruits = ['banana', 'orange', 'mango', 'lemon'] 
+# last_fruit = fruits[-1]
+# second_last = fruits[-2]
+# print(last_fruit)                                                          # lemon
+# print(second_last)                                                         # mango
 
-# Slicing items
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-all_fruits = fruits[0:4]
-print(all_fruits)
-# this is also give the same result as the above
-all_fruits = fruits[0:]                                                      # if we don't set where to stop it takes all the rest
-print(all_fruits)
-orange_and_mango = fruits[1:3]                                               # it does not include the end index
-orange_mango_lemon = fruits[1:]
-print(orange_and_mango)
-print(orange_mango_lemon)
+# # Slicing items
+# fruits = ['banana', 'orange', 'mango', 'lemon'] 
+# all_fruits = fruits[0:4]
+# print(all_fruits)
+# # this is also give the same result as the above
+# all_fruits = fruits[0:]                                                      # if we don't set where to stop it takes all the rest
+# print(all_fruits)
+# orange_and_mango = fruits[1:3]                                               # it does not include the end index
+# orange_mango_lemon = fruits[1:]
+# print(orange_and_mango)
+# print(orange_mango_lemon)
 
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-all_fruits = fruits[-4:]                                                     # it returns all the fruits
-# this is also give the same result as the above
-orange_and_mango = fruits[-3:-1]                                             # it does not include the end index
-orange_mango_lemon = fruits[-3:]
+# fruits = ['banana', 'orange', 'mango', 'lemon'] 
+# all_fruits = fruits[-4:]                                                     # it returns all the fruits
+# # this is also give the same result as the above
+# orange_and_mango = fruits[-3:-1]                                             # it does not include the end index
+# orange_mango_lemon = fruits[-3:]
 
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-fruits[0] = 'avocada'
-print(fruits)
-fruits[1] = 'apple'
-print(fruits)
+# fruits = ['banana', 'orange', 'mango', 'lemon'] 
+# fruits[0] = 'avocada'
+# print(fruits)
+# fruits[1] = 'apple'
+# print(fruits)
 
-last_index = len(fruits) - 1
-fruits[last_index] = 'lime'
-print(fruits)
+# last_index = len(fruits) - 1
+# fruits[last_index] = 'lime'
+# print(fruits)
 
-fruits[-1] = 'lime'
-print(fruits)
+# fruits[-1] = 'lime'
+# print(fruits)
 
-# checking items
+# # checking items
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# does_exist = 'banana' in fruits
+# print(does_exist)                                                           # True
+# does_exist = 'lime' in fruits
+# print(does_exist)                                                           # False
+
+# # append
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# fruits.append('apple')                                                      # ['banana', 'orange', 'mango', 'lemon', 'apple']
+# print(fruits)
+# fruits.append('lime')                                                       # ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime]
+# print(fruits)
+
+# # insert
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# fruits.insert(1,'apple')                                                    # insert apple between banana and orange
+# print(fruits)                                                               # ['banana', 'apple', 'orange', 'mango', 'lemon']
+# fruits.insert(3,'lime')                                                     # ['banana', 'apple', 'orange', 'lime', 'mango', 'lemon']
+# print(fruits)
+
+# # remove
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# fruits.remove('banana')
+# print(fruits)                                                               # ['orange', 'mango', 'lemon']
+# fruits.remove('lemon')
+# print(fruits)                                                               # ['orange', 'mango']
+
+# # pop 
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# fruits.pop()
+# print(fruits)                                                               # ['banana', 'orange', 'mango']
+# # we can pop based on the index too
+# fruits.pop(0)
+# print(fruits)                                                               # ['orange', 'mango'] 
+
+# del
 fruits = ['banana', 'orange', 'mango', 'lemon']
-does_exist = 'banana' in fruits
-print(does_exist)                                                           # True
-does_exist = 'lime' in fruits
-print(does_exist)                                                           # False
-
-# append
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.append('apple')                                                      # ['banana', 'orange', 'mango', 'lemon', 'apple']
-print(fruits)
-fruits.append('lime')                                                       # ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime]
-print(fruits)
-
-# insert
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.insert(1,'apple')                                                    # insert apple between banana and orange
-print(fruits)                                                               # ['banana', 'apple', 'orange', 'mango', 'lemon']
-fruits.insert(3,'lime')                                                     # ['banana', 'apple', 'orange', 'lime', 'mango', 'lemon']
-print(fruits)
-
-# remove
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.remove('banana')
-print(fruits)                                                               # ['orange', 'mango', 'lemon']
-fruits.remove('lemon')
-print(fruits)                                                               # ['orange', 'mango']
+del fruits[0]
+print(fruits)                                                                 # ['orange', 'mango', 'lemon']
