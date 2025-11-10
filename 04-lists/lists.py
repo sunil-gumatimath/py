39b4c3d4ff58384853a5e8737d6405906cd0dae5
SuniL <sunilgumatimath.vs@gmail.com>
2025-11-10 12:20:29 +0530

    refactor: enhance lists tutorial with sections, comments, and descriptive prints
    - Add header and section comments for better organization
    - Improve print statements to include descriptive labels
    - Add more examples for list operations like slicing and modification
    - Fix typo in 'avocado'

--- 04-lists/lists.py
+++ 04-lists/lists.py
@@ -1,199 +1,214 @@
-empty_list: list[int] = []
-print(len(empty_list))
+# Python Lists Tutorial
+# This script demonstrates various operations on Python lists
 
-fruits = ['banana', 'orange', 'mango', 'lemon']                         # list of fruits
-vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']          # list of vegetables
-animal_products = ['milk', 'meat', 'butter', 'yoghurt']                 # list of animal products
-web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongoDB']    # list of web technologies
+# Section 1: Creating Lists
+empty_list: list[int] = []
+print('Length of empty list:', len(empty_list))
+
+fruits = ['banana', 'orange', 'mango', 'lemon']  # list of fruits
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']  # list of vegetables
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']  # list of animal products
+web_techs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongoDB']  # list of web technologies
 countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
 
-# Print the lists and it length
+# Section 2: Printing Lists and Their Lengths
 print('Fruits:', fruits)
 print('Number of fruits:', len(fruits))
 print('Vegetables:', vegetables)
 print('Number of vegetables:', len(vegetables))
-print('Animal products:',animal_products)
+print('Animal products:', animal_products)
 print('Number of animal products:', len(animal_products))
 print('Web technologies:', web_techs)
 print('Number of web technologies:', len(web_techs))
 print('Number of countries:', len(countries))
-print('')
+print()
 
-
-# Modifying list
-fruits = ['banana','orange','mango','lemon']
+# Section 3: Accessing List Items
+fruits = ['banana', 'orange', 'mango', 'lemon']
 first_fruit = fruits[0]
-print(first_fruit)
+print('First fruit:', first_fruit)
 second_fruit = fruits[1]
-print(second_fruit)
+print('Second fruit:', second_fruit)
 last_fruit = fruits[-1]
-print(last_fruit)
+print('Last fruit:', last_fruit)
 
 # Last index
 last_index = len(fruits) - 1
-print(last_index)
+print('Last index:', last_index)
+print()
 
-print(' ')
-
-# Accessing items
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-last_fruit = fruits[-1]
-second_last = fruits[-2]
-print(last_fruit)                                                          # lemon
-print(second_last)                                                         # mango
-
-# Slicing items
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
+# Section 4: Slicing Lists
+fruits = ['banana', 'orange', 'mango', 'lemon']
 all_fruits = fruits[0:4]
-print(all_fruits)
-# this is also give the same result as the above
-all_fruits = fruits[0:]                                                      # if we don't set where to stop it takes all the rest
-print(all_fruits)
-orange_and_mango = fruits[1:3]                                               # it does not include the end index
+print('All fruits:', all_fruits)
+
+# This gives the same result
+all_fruits = fruits[0:]  # if we don't set where to stop it takes all the rest
+print('All fruits (alternative):', all_fruits)
+
+orange_and_mango = fruits[1:3]  # it does not include the end index
+print('Orange and mango:', orange_and_mango)
+
 orange_mango_lemon = fruits[1:]
-print(orange_and_mango)
-print(orange_mango_lemon)
+print('Orange, mango, lemon:', orange_mango_lemon)
 
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-all_fruits = fruits[-4:]                                                     # it returns all the fruits
-# this is also give the same result as the above
-orange_and_mango = fruits[-3:-1]                                             # it does not include the end index
+# Using negative indices
+all_fruits = fruits[-4:]  # it returns all the fruits
+print('All fruits (negative index):', all_fruits)
+
+orange_and_mango = fruits[-3:-1]  # it does not include the end index
+print('Orange and mango (negative):', orange_and_mango)
+
 orange_mango_lemon = fruits[-3:]
+print('Orange, mango, lemon (negative):', orange_mango_lemon)
 
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-fruits[0] = 'avocada'
-print(fruits)
+# Section 5: Modifying List Items
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits[0] = 'avocado'
+print('After changing first fruit:', fruits)
+
 fruits[1] = 'apple'
-print(fruits)
+print('After changing second fruit:', fruits)
 
 last_index = len(fruits) - 1
 fruits[last_index] = 'lime'
-print(fruits)
+print('After changing last fruit (by index):', fruits)
 
 fruits[-1] = 'lime'
-print(fruits)
+print('After changing last fruit (negative index):', fruits)
 
-# checking items
+# Section 6: Checking if an Item Exists in a List
 fruits = ['banana', 'orange', 'mango', 'lemon']
 does_exist = 'banana' in fruits
-print(does_exist)                                                           # True
+print('Does banana exist in fruits?', does_exist)  # True
+
 does_exist = 'lime' in fruits
-print(does_exist)                                                           # False
+print('Does lime exist in fruits?', does_exist)  # False
 
-# append
+# Section 7: Adding Items to a List
+# Append
 fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.append('apple')                                                      # ['banana', 'orange', 'mango', 'lemon', 'apple']
-print(fruits)
-fruits.append('lime')                                                       # ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime]
-print(fruits)
+fruits.append('apple')
+print('After appending apple:', fruits)
 
-# insert
+fruits.append('lime')
+print('After appending lime:', fruits)
+
+# Insert
 fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.insert(1,'apple')                                                    # insert apple between banana and orange
-print(fruits)                                                               # ['banana', 'apple', 'orange', 'mango', 'lemon']
-fruits.insert(3,'lime')                                                     # ['banana', 'apple', 'orange', 'lime', 'mango', 'lemon']
-print(fruits)
+fruits.insert(1, 'apple')  # insert apple between banana and orange
+print('After inserting apple at index 1:', fruits)
 
-# remove
+fruits.insert(3, 'lime')  # insert lime at index 3
+print('After inserting lime at index 3:', fruits)
+
+# Section 8: Removing Items from a List
+# Remove
 fruits = ['banana', 'orange', 'mango', 'lemon']
 fruits.remove('banana')
-print(fruits)                                                               # ['orange', 'mango', 'lemon']
+print('After removing banana:', fruits)
+
 fruits.remove('lemon')
-print(fruits)                                                               # ['orange', 'mango']
+print('After removing lemon:', fruits)
 
-# pop 
+# Pop
 fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.pop()
-print(fruits)                                                               # ['banana', 'orange', 'mango']
-# we can pop based on the index too
-fruits.pop(0)
-print(fruits)                                                               # ['orange', 'mango'] 
+fruits.pop()  # removes the last item
+print('After popping last item:', fruits)
 
-# del
+fruits.pop(0)  # pop based on index
+print('After popping first item:', fruits)
+
+# Del
 fruits = ['banana', 'orange', 'mango', 'lemon']
 del fruits[0]
-print(fruits)                                                                 # ['orange', 'mango', 'lemon']
+print('After deleting first item:', fruits)
 
 del fruits[1]
-print(fruits)                                                                 # ['orange', 'lemon']
+print('After deleting second item:', fruits)
 
 del fruits
-print(fruits)                                                                 # NameError: name 'fruits' is not defined
+print(fruits)     # NameError: name 'fruits' is not defined
 
-# clear
+# Clear
 fruits = ['banana', 'orange', 'mango', 'lemon']
 fruits.clear()
-print(fruits)
+print('After clearing the list:', fruits)
 
-# copying a lists
+# Section 9: Copying a List
 fruits = ['banana', 'orange', 'mango', 'lemon']
 fruits_copy = fruits.copy()
-fruits_copy_1 = fruits_copy[:]
-print(fruits_copy)            
-print(fruits_copy_1)                                                  # ['banana', 'orange', 'mango', 'lemon']
+print('Copied list:', fruits_copy)
 
-#  join
-positive_numbers = [1,2,3,4,5]
+fruits_copy_1 = fruits[:]
+print('Copied list (slicing):', fruits_copy_1)
+
+# Section 10: Joining Lists
+# Using concatenation
+positive_numbers = [1, 2, 3, 4, 5]
 zero = [0]
-negative_numbers = [-5,-4,-3,-2,-1]
+negative_numbers = [-5, -4, -3, -2, -1]
 integers = positive_numbers + zero + negative_numbers
-print(integers)                                                        # [1, 2, 3, 4, 5, 0, -5, -4, -3, -2, -1]
+print('Joined integers:', integers)
 
 fruits = ['banana', 'orange', 'mango', 'lemon']
-vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot'] 
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
 fruits_and_vegetables = fruits + vegetables
-print(fruits_and_vegetables)                                           # ['banana', 'orange', 'mango', 'lemon', 'Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+print('Fruits and vegetables:', fruits_and_vegetables)
 
-# join with extend
-num1 = [0,1,2,3]
-num2 = [4,5,6]
-num1.extend(num2)                                                        # Number : [0, 1, 2, 3, 4, 5, 6]
-print('Number :',num1)                      
+# Using extend
+num1 = [0, 1, 2, 3]
+num2 = [4, 5, 6]
+num1.extend(num2)
+print('Numbers after extend:', num1)
 
-negative_numbers = [-5,-4,-3,-2,-1]
-positive_numbers = [1, 2, 3,4,5]
+negative_numbers = [-5, -4, -3, -2, -1]
+positive_numbers = [1, 2, 3, 4, 5]
 zero = [0]
 
 negative_numbers.extend(zero)
-negative_numbers.extend(positive_numbers)                               # Integers :  [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
-print('Integers : ',negative_numbers)
+negative_numbers.extend(positive_numbers)
+print('Integers after extend:', negative_numbers)
 
 fruits = ['banana', 'orange', 'mango', 'lemon']
-vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
 fruits.extend(vegetables)
-print('Fruits and vegetables:', fruits)                                 # Fruits and vegetables: ['banana', 'orange', 'mango', 'lemon', 'Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+print('Fruits and vegetables after extend:', fruits)
 
-# count
+# Section 11: Counting Items in a List
 fruits = ['banana', 'orange', 'mango', 'lemon']
-print(fruits.count('orange'))
+print('Count of orange in fruits:', fruits.count('orange'))
 
 ages = [22, 19, 24, 25, 26, 24, 25, 24]
-print(ages.count(24))
+print('Count of 24 in ages:', ages.count(24))
 
-# index
+# Section 12: Finding Index of an Item
 fruits = ['banana', 'orange', 'mango', 'lemon']
-print(fruits.index('mango'))
+print('Index of mango in fruits:', fruits.index('mango'))
 
 ages = [22, 19, 24, 25, 26, 24, 25, 24]
-print(ages.index(25))
+print('Index of 25 in ages:', ages.index(25))
 
-# Reverse
+# Section 13: Reversing a List
 fruits = ['banana', 'orange', 'mango', 'lemon']
 fruits.reverse()
-print(fruits)
+print('Reversed fruits:', fruits)
+
 ages = [22, 19, 24, 25, 26, 24, 25, 24]
 ages.reverse()
-print(ages)
+print('Reversed ages:', ages)
 
-# sort
+# Section 14: Sorting List Items
 fruits = ['banana', 'orange', 'mango', 'lemon']
 fruits.sort()
-print(fruits)
+print('Sorted fruits (ascending):', fruits)
+
 fruits.sort(reverse=True)
-print(fruits)
+print('Sorted fruits (descending):', fruits)
 
 ages = [22, 19, 24, 25, 26, 24, 25, 24]
 ages.sort()
-print(ages) 
+print('Sorted ages (ascending):', ages)
+
 ages.sort(reverse=True)
-print(ages) 
+print('Sorted ages (descending):', ages)
