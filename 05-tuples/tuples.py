04ed503caa90b95f3115d6eb8866ffd85fb60b9a
SuniL <sunilgumatimath.vs@gmail.com>
2025-11-11 16:46:54 +0530

    feat(tuples): add negative index slicing examples and fruit tuple demos

--- 05-tuples/tuples.py
+++ 05-tuples/tuples.py
@@ -52,3 +52,21 @@ middle_two_items = tpl[1:3]
 
 print(all_items)
 print(middle_two_items)
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[0:4]    # all items
+all_fruits= fruits[0:]      # all items
+orange_mango = fruits[1:3]  # doesn't include item at index 3
+orange_to_the_rest = fruits[1:]
+
+# Syntax
+tpl = ('item1', 'item2', 'item3','item4')
+all_items = tpl[-4:]         # all items
+middle_two_items = tpl[-3:-1]  # does not include item at index 3 (-1)
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[-4:]    # all items
+orange_mango = fruits[-3:-1]  # doesn't include item at index 3
+orange_to_the_rest = fruits[-3:]
