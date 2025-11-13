e10518081ff7b5f59825215b08ddb5899fc39177
SuniL <sunilgumatimath.vs@gmail.com>
2025-11-13 14:56:23 +0530

    feat(sets): enhance set operations examples and add length checking

--- 06_sets/sets.py
+++ 06_sets/sets.py
@@ -1,3 +1,4 @@
+# We use the set() built-in function.
 st = set()
 print(st)
 print(type(st))
@@ -9,3 +10,21 @@ print(st)
 # syntax
 fruits = {'banana', 'orange', 'mango', 'lemon'}
 print(fruits)
+
+
+# We use len() method to find the length of a set.
+# syntax
+st = {'item1', 'item2', 'item3', 'item4'}
+len(st)
+
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+print(len(fruits))
+
+# Checking an Item
+# To check if an item exist in a list we use in membership operator.
+# syntax
+st = {'item1', 'item2', 'item3', 'item4'}
+print("Does set st conatin item 3 : ", 'item3' in st) # Does set st contain item3? True
+
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+print('mango' in fruits)        # True
