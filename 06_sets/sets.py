04605c3a89c9ed20ccfc034010d68fa0f0ebfff1
SuniL <sunilgumatimath.vs@gmail.com>
2025-11-14 14:34:33 +0530

    feat(sets): add examples for symmetric difference and disjoint checks
    Introduce new sections in sets.py with code examples for symmetric difference and isdisjoint methods, including mathematical notes and practical demonstrations using predefined sets, to illustrate advanced set operations in Python. This enhances the tutorial's coverage of set manipulation techniques.

--- 06_sets/sets.py
+++ 06_sets/sets.py
@@ -163,3 +163,35 @@ result = python.difference(dragon)     # {'p', 'y', 't'}  - the result is unorde
 print(result)
 result = dragon.difference(python)     # {'d', 'r', 'a', 'g'}
 print(result)
+
+
+# Finding Symmetric Difference Between Two Sets
+# It returns the symmetric difference between two sets. It means that it returns a set that contains 
+# all items from both sets, except items that are present in both sets, mathematically: (A\B) ∪ (B\A)
+# syntax
+st1 = {'item1', 'item2', 'item3', 'item4'}
+st2 = {'item2', 'item3'}
+# it means (A\B)∪(B\A)
+st2.symmetric_difference(st1) # {'item1', 'item4'}
+
+whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+some_numbers = {1, 2, 3, 4, 5}
+whole_numbers.symmetric_difference(some_numbers) # {0, 6, 7, 8, 9, 10}
+
+python = {'p', 'y', 't', 'h', 'o','n'}
+dragon = {'d', 'r', 'a', 'g', 'o','n'}
+python.symmetric_difference(dragon)  # {'r', 't', 'p', 'y', 'g', 'a', 'd', 'h'}
+
+# Joining Sets
+# syntax
+st1 = {'item1', 'item2', 'item3', 'item4'}
+st2 = {'item2', 'item3'}
+st2.isdisjoint(st1) # False
+
+even_numbers = {0, 2, 4 ,6, 8}
+odd_numbers = {1, 3, 5, 7, 9}
+even_numbers.isdisjoint(odd_numbers) # True, because no common item
+
+python = {'p', 'y', 't', 'h', 'o','n'}
+dragon = {'d', 'r', 'a', 'g', 'o','n'}
+python.isdisjoint(dragon)  # False, there are common items {'o', 'n'}
