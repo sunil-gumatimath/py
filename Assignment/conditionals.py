396e6113b71a90639c72e7277f935a1ceb0878e0
SuniL <sunilgumatimath.vs@gmail.com>
2025-12-04 13:31:16 +0530

    feat: update person dictionary and enhance skill evaluation logic

--- Assignment/conditionals.py
+++ Assignment/conditionals.py
@@ -102,16 +102,13 @@
 # Here we have a person dictionary. Feel free to modify it!
 
 person = {
-    "first_name": "luffy",  
-    "last_name": "nika",    
-    "age": 22,              
-    "country": "Foosha",    
-    "is_marred": False,     
-    "skills": ["JavaScript", "React", "Node", "MongoDB", "Python"], 
-    "address": {
-        "street": "Space street",
-        "zipcode": "02210"
-    },      
+    "first_name": "luffy",
+    "last_name": "nika",
+    "age": 22,
+    "country": "Foosha",
+    "is_marred": True,
+    "skills": ["JavaScript", "React", "Node", "MongoDB", "Python"],
+    "address": {"street": "Space street", "zipcode": "02210"},
 }
 
 #  Check if the person dictionary has skills key, if so print out the middle skill in the skills list.
@@ -119,16 +116,34 @@ if "skills" in person:
     skills = person["skills"]
     middle_idx = len(skills) // 2
     print(skills[middle_idx])
-    
+
 #  Check if the person dictionary has skills key, if so check if the person has 'Python' skill and print out the result.
 if "skills" in person:
     skills = person["skills"]
     print("Python" in skills)
 
-#  If a person skills has only JavaScript and React, print('He is a front end developer'), if the person skills has Node, Python, MongoDB, 
-# print('He is a backend developer'), if the person skills has React, Node and MongoDB, Print('He is a fullstack developer'), 
+#  If a person skills has only JavaScript and React, print('He is a front end developer'), if the person skills has Node, Python, MongoDB,
+# print('He is a backend developer'), if the person skills has React, Node and MongoDB, Print('He is a fullstack developer'),
 # else print('unknown title') - for more accurate results more conditions can be nested!
 
+skills = person["skills"]
+skills_set = set(skills)
+
+frontend = {"JavaScript", "React"}
+backend = {"Node", "Python", "MongoDB"}
+fullstack = {"React", "Node", "MongoDB"}
+
+if fullstack.issubset(skills_set):
+    print("He is a fullstack developer")
+elif backend.issubset(skills_set):
+    print("He is a backend developer")
+elif frontend.issubset(skills_set):
+    print("He is a front end developer")
+else:
+    print("unknown title")
 
 #  If the person is married and if he lives in Foosha, print the information in the following format:
-    # Asabeneh Yetayeh lives in Finland. He is married.
+# luffy nika lives in Foosha. He is married.
+
+if person["is_marred"] == True and person["country"] == "Foosha":
+    print(f'{person["first_name"]} {person["last_name"]} lives in {person["country"]}. He is married')
