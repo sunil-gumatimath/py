e1a67e267079e10f7fb1fb0603f4518043b05022
SuniL <sunilgumatimath.vs@gmail.com>
2025-12-08 23:47:34 +0530

    feat : added pass , continue and range function using loops

--- 09_Loops/for_loops.py
+++ 09_Loops/for_loops.py
@@ -55,3 +55,71 @@ for number in numbers:
     print(number)
     if number == 3:
         break
+
+# Continue: We use continue when we like to skip some of the steps in the iteration of the loop.
+numbers = (0,1,2,3,4,5)
+for number in numbers:
+    print(number)
+    if number == 3:
+        continue
+    print('Next number should be ',number + 1) if number != 5 else print("loop's end ")
+print('Outside the loops')
+
+# The Range Function
+
+# The range() function is used list of numbers. The range(start, end, step) takes three parameters: starting, ending and increment. 
+# By default it starts from 0 and the increment is 1. The range sequence needs at least 1 argument (end). Creating sequences using range
+
+lst = list(range(11))
+print(lst)
+
+st = set(range(1,11))
+print(st)
+
+lst = list(range(0,11,2))
+print(lst)
+st = set(range(0,11,2))
+print(st)
+
+# syntax
+# for iterator in range(start, end, step):
+
+# Nested For Loop
+# We can write loops inside a loop.
+
+person = {
+    "first_name": "luffy",
+    "last_name": "nika",
+    "age": 24,
+    "country": "OnePiece",
+    "is_married": False,
+    "skills": ["gear1", "gear2", "gear3", "gear4"],
+    "address": {"stree": "BTM", "city": "BNG"},
+}
+
+for key in person:
+    if key == 'skills':
+        for skill in person["skills"]:
+            print(skill)
+
+# For Else
+# If we want to execute some message when the loop ends, we use else.
+
+# syntax
+# for iterator in range(start, end, step):
+#     do something
+# else:
+#     print('The loop ended')
+
+for number in range(0,11):
+    print(number)
+else:
+    print('the loops end')
+    
+# Pass
+# In python when statement is required (after semicolon), but we don't like to execute any code there, we can write the word pass to avoid errors. 
+# Also we can use it as a placeholder, for future statements.
+
+for number in range(6):
+    pass
+
